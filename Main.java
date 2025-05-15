1df118904bb949702c209373ac63edf0d80b1d91
cheesebob12 <134903358+cheesebob12@users.noreply.github.com>
2025-05-15 17:37:55 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,21 +1,21 @@
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
-class Main {
+public class Main {
 	public static void main(String[] args) {
-		// DECLARATION + INITIALIZATION
 		int choice = -1;
 		boolean tryAgain = true;
 		Scanner keyboard = new Scanner(System.in);
+
 		Pokemon[] caught = {
-			new Pokemon("Pikachu", "Electric"),
-			new Pokemon("Bulbasaur", "Grass", "Poison"),
-			new Pokemon("Charmeleon", "Fire"),
-			new Pokemon("Squirtle", "Water"),
-			new Pokemon("Butterfree", "Bug", "Flying"),
-			new Pokemon("Pidgeotto", "Normal", "Flying")
+				new Pokemon("Pikachu", "Electric"),
+				new Pokemon("Bulbasaur", "Grass", "Poison"),
+				new Pokemon("Charmeleon", "Fire"),
+				new Pokemon("Squirtle", "Water"),
+				new Pokemon("Butterfree", "Bug", "Flying"),
+				new Pokemon("Pidgeotto", "Normal", "Flying")
 		};
 
-		// WELCOME
 		System.out.println("Preloading Pokemon Box...");
 		PokemonBox myBox = new PokemonBox(caught);
 		System.out.println("...Done!\n");
@@ -25,36 +25,56 @@ class Main {
 		System.out.println("---------------------------\n");
 		System.out.println(myBox);
 
-		//INPUT + PROCESSING + OUTPUT
 		do {
 			System.out.println("\nMAIN MENU\nWhat would you like to do?");
 			System.out.println("\t1) Add a New Pokemon \n\t2) List All Pokemon \n\t3) Exit Program \n");
 			System.out.print("Enter choice number> ");
-			choice = keyboard.nextInt(); //could throw exception here and skip rest of code
-			keyboard.nextLine();
-			System.out.println();
 
-			if (choice == 1) {
-				System.out.println("Enter Pokemon Info to be added:");
-				System.out.print("Enter Pokemon Name> ");
-				String name = keyboard.nextLine();
-				System.out.print("Enter Pokemon Type #1> ");
-				String type1 = keyboard.nextLine();
-				System.out.print("Enter Pokemon Type #2 (none if no second type)> ");
-				String type2 = keyboard.nextLine();
-				type2 = (type2.equalsIgnoreCase("none")) ? null : type2;
+			try {
+				choice = keyboard.nextInt();
+				keyboard.nextLine(); // clear newline
+				System.out.println();
 
-				Pokemon p = new Pokemon(name, type1, type2);
-				myBox.add(p); //could throw exception here and skip rest of code
+				if (choice == 1) {
+					boolean validInput = false;
+					while (!validInput) {
+						try {
+							System.out.println("Enter Pokemon Info to be added:");
+							System.out.print("Enter Pokemon Name> ");
+							String name = keyboard.nextLine();
+							System.out.print("Enter Pokemon Type #1> ");
+							String type1 = keyboard.nextLine();
+							System.out.print("Enter Pokemon Type #2 (none if no second type)> ");
+							String type2 = keyboard.nextLine();
+							type2 = (type2.equalsIgnoreCase("none")) ? null : type2;
 
-				System.out.println("\n" + name + " added!");
-			} else if (choice == 2) {
-				System.out.println(myBox);
-			} else if (choice == 3) {
-				keyboard.close();
-				tryAgain = false;
-			} else {
-				System.out.println("Invalid choice, please pick a valid option from the menu.\n");
+							Pokemon p = new Pokemon(name, type1, type2);
+
+							myBox.add(p); // can throw PokemonAlreadyExistsException
+							System.out.println("\n" + name + " added!");
+							validInput = true;
+
+						} catch (IllegalArgumentException e) {
+							System.out.println("Invalid Pokemon data: " + e.getMessage());
+							System.out.println("Please re-enter the Pokemon information.\n");
+						} catch (PokemonAlreadyExistsException e) {
+							System.out.println("Error: " + e.getMessage());
+							System.out.println("Please try adding a different Pokemon.\n");
+						}
+					}
+
+				} else if (choice == 2) {
+					System.out.println(myBox);
+				} else if (choice == 3) {
+					keyboard.close();
+					tryAgain = false;
+				} else {
+					System.out.println("Invalid choice, please pick a valid option from the menu.\n");
+				}
+
+			} catch (InputMismatchException e) {
+				System.out.println("Invalid input. Please enter a number from the menu options.\n");
+				keyboard.nextLine(); // clear invalid input
 			}
 		} while (tryAgain);
 
--- PokemonBox.java
+++ PokemonBox.java
@@ -1,96 +1,79 @@
+
 public class PokemonBox {
+    public static final int DEFAULT_CAPACITY = 10;
 
-	// CONSTANT VARIABLES
-	public static final int DEFAULT_CAPACITY = 10;
+    private Pokemon[] caught;
+    private int numCaught;
 
-	// INSTANCE VARIABLES
-	private Pokemon[] caught;
-	private int numCaught;
+    public PokemonBox(Pokemon[] caught) {
+        if(caught == null || caught.length == 0) {
+            throw new IllegalArgumentException("Invalid Pokemon array provided.");
+        }
+        this.numCaught = caught.length;
+        this.caught = this.deepCopyArray(caught, this.numCaught * 2);
+    }
 
-	// CONSTRUCTORS
-	public PokemonBox(Pokemon[] caught) {
-		if(caught == null || caught.length == 0) {
-			System.out.println("ERROR: Invalid Pokemon array provided to PokemonBox. Exiting program.");
-			System.exit(0);
-		}
-		this.numCaught = caught.length;
-		this.caught = this.deepCopyArray(caught, this.numCaught*2);
-	}
+    public PokemonBox() {
+        this.caught = new Pokemon[DEFAULT_CAPACITY];
+        this.numCaught = 0;
+    }
 
-	public PokemonBox() {
-		this.caught = new Pokemon[DEFAULT_CAPACITY];
-		this.numCaught = 0;
-	}
+    public int getLocation(String pokemonName) {
+        for (int i = 0; i < this.numCaught; i++) {
+            if (this.caught[i].getName().equalsIgnoreCase(pokemonName)) {
+                return i;
+            }
+        }
+        return -1;
+    }
 
-	// ACCESSOR/GETTER METHODS
-	//returns -1 if not found
-	public int getLocation(String pokemonName) {
-		int location = -1, count = 0;
-		//loop until we reach end of array or we find item
-		while(count < this.numCaught && location == -1) {
-			if(this.caught[count].getName().equalsIgnoreCase(pokemonName)) {
-				//found pokemon!
-				location = count;
-			} else {
-				//didnt find pokemon, setup for next loop
-				count++;
-			}
-		}
-		return location;
-	}
+    public Pokemon getPokemon(int location) {
+        if (location < 0 || location >= numCaught) {
+            throw new IndexOutOfBoundsException("Invalid index for getPokemon");
+        }
+        return this.caught[location];
+    }
 
-	public Pokemon getPokemon(int location) {
-		return this.caught[location];
-	}
+    public int getNumCaught() {
+        return this.numCaught;
+    }
 
-	public int getNumCaught() {
-		return this.numCaught;
-	}
+    public boolean isEmpty() {
+        return this.numCaught == 0;
+    }
 
-	public boolean isEmpty() {
-		return this.numCaught == 0;
-	}
+    public boolean hasPokemon(String pokemonName) {
+        return this.getLocation(pokemonName) != -1;
+    }
 
-	public boolean hasPokemon(String pokemonName) {
-		return this.getLocation(pokemonName) != -1;
-	}
+    public void add(Pokemon newPoke) throws PokemonAlreadyExistsException {
+        if (this.hasPokemon(newPoke.getName())) {
+            throw new PokemonAlreadyExistsException("This Pokemon already exists in your box.");
+        }
+        if (this.numCaught == this.caught.length) {
+            this.caught = this.deepCopyArray(this.caught, this.numCaught * 2);
+        }
+        this.caught[this.numCaught] = new Pokemon(newPoke);
+        this.numCaught++;
+    }
 
-	// MUTATOR/SETTER METHODS
-	public void add(Pokemon newPoke) {
-		//new pokemon,  add to partially filled array
-		//but first check if box is full
-		if(this.numCaught == this.caught.length) {
-			//if full, then grow array *2 and copy contents over
-			this.caught = this.deepCopyArray(this.caught, this.numCaught*2);
-		}
+    public String toString() {
+        if(this.isEmpty()) {
+            return "This box is empty";
+        } else {
+            String all = "\t01. " + this.caught[0].toRow();
+            for(int i = 1; i < this.numCaught; i++) {
+                all += String.format("%n\t%02d. %s", i+1, this.caught[i].toRow());
+            }
+            return String.format("This box has %d Pokemon, which are:%n%s", this.numCaught, all);
+        }
+    }
 
-		//then add new caught pokemon
-		this.caught[this.numCaught] = new Pokemon(newPoke);
-		this.numCaught++;
-	}
-	
-	// OTHER REQUIRED METHODS
-	public String toString() {
-		if(this.isEmpty()) {
-			return "This box is empty";
-		} else {
-			String all = "\t01. " + this.caught[0].toRow();
-			for(int i = 1; i < this.numCaught; i++) {
-				all += String.format("%n\t%02d. %s", i+1, this.caught[i].toRow());
-			}
-
-			return String.format("This box has %d Pokemon, which are:%n%s",
-				this.numCaught, all);
-		}
-	}
-
-	private Pokemon[] deepCopyArray(Pokemon[] p, int newLength) {
-		Pokemon[] deepCopy = new Pokemon[newLength];
-		
-		for(int i = 0; i < p.length; i++) {
-			deepCopy[i] = new Pokemon(p[i]);
-		}
-
-		return deepCopy;
-	}
+    private Pokemon[] deepCopyArray(Pokemon[] p, int newLength) {
+        Pokemon[] deepCopy = new Pokemon[newLength];
+        for(int i = 0; i < p.length && p[i] != null; i++) {
+            deepCopy[i] = new Pokemon(p[i]);
+        }
+        return deepCopy;
+    }
 }
